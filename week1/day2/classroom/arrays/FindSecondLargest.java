5df9368348a8d02f46bf405486bbd3f5f36fae10
ManonRaj <114816368+ManonRaj@users.noreply.github.com>
2022-10-07 08:54:10 +0530

    Update FindSecondLargest.java
    Manon's code

--- week1/day2/classroom/arrays/FindSecondLargest.java
+++ week1/day2/classroom/arrays/FindSecondLargest.java
@@ -5,20 +5,16 @@ import java.util.Arrays;
 public class FindSecondLargest {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] data = {3,2,11,4,6,7};		
+		int total = 6;
+		Arrays.sort(data);
+		//String value=Arrays.toString(data);
+		System.out.println("Sorted Array is:" + Arrays.toString(data));
+		System.out.println("second Largest number is :" + data[total-2]);
 
-		// Here is the input
-		int[] data = {3,2,11,4,6,7};
-
-		
-		/*
-		 Pseudo Code:
-		 1) Arrange the array in ascending order
-		 2) Pick the 2nd element from the last and print it
-		 */
-		
-
-		
 	}
+
 
 	
 }
